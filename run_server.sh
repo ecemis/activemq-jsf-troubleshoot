2e11a8a10c43d8fd7020391e35a5c0b27a28bfa8
Ihsan Ecemis <miecemis@gmail.com>
2016-04-01 22:28:18 -0400

    Non SSL fetching works with ActiveMQ broker

--- run_server.sh
+++ run_server.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-mvn -Dmaven.test.skip=true -Dtomee-plugin.https=8443 clean package tomee:build tomee:run
+mvn -Dmaven.test.skip=true -Dtomee-plugin.http=8443 clean package tomee:build tomee:run
 
