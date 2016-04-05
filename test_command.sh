0bd87a3ecc47e4c690246ddfe920102c32f88a2a
Ihsan Ecemis <miecemis@gmail.com>
2016-04-04 21:44:12 -0400

    Added a static file for the non-working case

--- test_command.sh
+++ test_command.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-curl -v --insecure "http://localhost:8443/jsf/javax.faces.resource/primefaces.js?ln=primefaces&v=5.3"
+curl -v --insecure "https://localhost:8443/jsf/javax.faces.resource/primefaces.js?ln=primefaces&v=5.3"
 
---
+++ test_static_file_command.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+curl -v --insecure "https://localhost:8443/sample_static_file.txt"
+
